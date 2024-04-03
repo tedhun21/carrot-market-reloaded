e51d1875537d6dca120d097646fed209c2f375c3
JiHun <82702997+tedhun21@users.noreply.github.com>
2024-04-03 16:50:36 +0900

    #5.7 Authentication - matcher

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,19 @@
 import { cookies } from "next/headers";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
+  const pathname = request.nextUrl.pathname;
+  if (pathname === "/") {
+    const response = NextResponse.next();
+    response.cookies.set("middleware-cookie", "hello");
+    return response;
+  }
   if (request.nextUrl.pathname === "/profile") {
     return Response.redirect(new URL("/", request.url));
   }
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
