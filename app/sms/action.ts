f9f662e438701f9c81e26c988bb41bc25c8b8927
JiHun <82702997+tedhun21@users.noreply.github.com>
2024-04-04 19:16:17 +0900

    #6.4 Social Authentication - SMS Token

--- app/sms/action.ts
+++ app/sms/action.ts
@@ -3,6 +3,8 @@
 import { z } from "zod";
 import validator from "validator";
 import { redirect } from "next/navigation";
+import crypto from "crypto";
+import db from "@/lib/db";
 
 const phoneSchema = z
   .string()
@@ -17,16 +19,57 @@ interface ActionState {
   token: boolean;
 }
 
+async function getToken() {
+  const token = crypto.randomInt(100000, 999999).toString();
+  const exists = await db.sMSToken.findUnique({
+    where: { token },
+    select: {
+      id: true,
+    },
+  });
+  if (exists) {
+    return getToken();
+  } else {
+    return token;
+  }
+}
+
 export async function smsLogIn(prevState: ActionState, formData: FormData) {
   const phone = formData.get("phone");
   const token = formData.get("token");
 
   if (!prevState.token) {
     const result = phoneSchema.safeParse(phone);
-    console.log(result);
+
     if (!result.success) {
       return { token: false, error: result.error.flatten() };
     } else {
+      // delete previous token
+      await db.sMSToken.deleteMany({
+        where: {
+          user: {
+            phone: result.data,
+          },
+        },
+      });
+      // create token
+      const token = await getToken();
+      await db.sMSToken.create({
+        data: {
+          token,
+          user: {
+            connectOrCreate: {
+              where: { phone: result.data },
+              create: {
+                username: crypto.randomBytes(10).toString("hex"),
+                phone: result.data,
+              },
+            },
+          },
+        },
+      });
+      // send the token using sms
+
       return { token: true };
     }
   } else {
