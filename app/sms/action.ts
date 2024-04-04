900282da6ac4e383ad517f4183d317b46a69b727
JiHun <82702997+tedhun21@users.noreply.github.com>
2024-04-04 19:45:40 +0900

    #6.5 Social Authentication - Token Verification

--- app/sms/action.ts
+++ app/sms/action.ts
@@ -5,6 +5,7 @@ import validator from "validator";
 import { redirect } from "next/navigation";
 import crypto from "crypto";
 import db from "@/lib/db";
+import { LoginSession } from "../github/utils";
 
 const phoneSchema = z
   .string()
@@ -13,7 +14,21 @@ const phoneSchema = z
     message: "Wrong phone format",
   });
 
-const tokenSchema = z.coerce.number().min(100000).max(999999);
+async function tokenExists(token: number) {
+  const exists = await db.sMSToken.findUnique({
+    where: {
+      token: token.toString(),
+    },
+    select: { id: true },
+  });
+  return Boolean(exists);
+}
+
+const tokenSchema = z.coerce
+  .number()
+  .min(100000)
+  .max(999999)
+  .refine(tokenExists, "This token does not exist.");
 
 interface ActionState {
   token: boolean;
@@ -73,10 +88,20 @@ export async function smsLogIn(prevState: ActionState, formData: FormData) {
       return { token: true };
     }
   } else {
-    const result = tokenSchema.safeParse(token);
+    const result = await tokenSchema.spa(token);
     if (!result.success) {
       return { token: true, error: result.error.flatten() };
     } else {
+      // get the userId of token
+      const token = await db.sMSToken.findUnique({
+        where: { token: result.data.toString() },
+        select: { id: true, userId: true },
+      });
+      // log the user in
+
+      await db.sMSToken.delete({ where: { id: token!.id } });
+      return LoginSession(token!.userId);
+
       redirect("/");
     }
   }
