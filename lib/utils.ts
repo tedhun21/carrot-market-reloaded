382dc06c9f7fa122d09d8423d8a337aab0b8da8d
tedhun21 <songjihun940409@gmail.com>
2024-04-12 13:22:50 +0900

    #7.3 Products - Detail Skeleton

---
+++ lib/utils.ts
@@ -0,0 +1,13 @@
+export function formatToTimeAgo(date: string) {
+  const dayInMs = 1000 * 60 * 60 * 24;
+  const time = new Date(date).getTime();
+  const now = new Date().getTime();
+  const diff = Math.round((time - now) / dayInMs);
+
+  const formatter = new Intl.RelativeTimeFormat("ko");
+
+  return formatter.format(diff, "day");
+}
+export function formatToWon(price: number) {
+  return price.toLocaleString("ko-KR");
+}
