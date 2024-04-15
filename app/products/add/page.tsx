e41fe536a563f25fac5d70fcea51735377573096
tedhun21 <songjihun940409@gmail.com>
2024-04-15 15:28:39 +0900

    #8.1 Product Upload - Form Action

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -3,20 +3,44 @@
 import Button from "@/components/button";
 import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
+import React, { useState } from "react";
+import { uploadProduct } from "./action";
 
 export default function AddProduct() {
   const [preview, setPreview] = useState("");
-  const onImageChange = () => {};
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = event;
+    if (!files) {
+      return;
+    }
+    console.log(files);
+    // 유저가 이미지를 올렸는지 확인할 것
+    const file = files[0];
+    // 4mb 이하를 올렸는지 확인할 것
+    if (file.size > 4194304) {
+      return;
+    }
+    const url = URL.createObjectURL(file);
+    setPreview(url);
+  };
   return (
     <div>
-      <form className="flex flex-col gap-5 p-5">
+      <form action={uploadProduct} className="flex flex-col gap-5 p-5">
         <label
           htmlFor="photo"
-          className="flex aspect-square cursor-pointer flex-col items-center justify-center rounded-md border-2 border-dashed border-neutral-300 text-neutral-300"
+          className="flex aspect-square cursor-pointer flex-col items-center justify-center rounded-md border-2 border-dashed border-neutral-300 bg-cover bg-center text-neutral-300"
+          style={{ backgroundImage: `url(${preview})` }}
         >
-          <PhotoIcon className="w-20" />
-          <div className="text-sm text-neutral-400">사진을 추가해주세요.</div>
+          {preview === "" ? (
+            <>
+              <PhotoIcon className="w-20" />
+              <div className="text-sm text-neutral-400">
+                사진을 추가해주세요.
+              </div>
+            </>
+          ) : null}
         </label>
         <input
           onChange={onImageChange}
