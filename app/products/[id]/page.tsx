673ed4ef43037f9fe42d69b5da7fd685c2ee731a
tedhun21 <songjihun940409@gmail.com>
2024-04-27 12:41:58 +0900

    #10.2 Cache - revalidateTag

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,10 +1,13 @@
-import db from "@/lib/db";
-import getSession from "@/lib/session";
-import { formatToWon } from "@/lib/utils";
-import { UserIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import { unstable_cache as nextCache, revalidateTag } from "next/cache";
+
+import { UserIcon } from "@heroicons/react/24/solid";
+
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { formatToWon } from "@/lib/utils";
 
 async function getIsOwner(userId: number) {
   const session = await getSession();
@@ -15,6 +18,7 @@ async function getIsOwner(userId: number) {
 }
 
 export async function getProduct(id: number) {
+  console.log("product");
   const product = await db.product.findUnique({
     where: { id },
     include: {
@@ -29,8 +33,25 @@ export async function getProduct(id: number) {
   return product;
 }
 
+const getCachedProduct = nextCache(getProduct, ["product-detail"], {
+  tags: ["product-detail"],
+});
+
+export async function getProductTitle(id: number) {
+  console.log("title");
+  const product = await db.product.findUnique({
+    where: { id },
+    select: { title: true },
+  });
+  return product;
+}
+
+const getCachedProductTitle = nextCache(getProductTitle, ["product-title"], {
+  tags: ["product-title"],
+});
+
 export async function generateMetadata({ params }: { params: { id: string } }) {
-  const product = await getProduct(Number(params.id));
+  const product = await getCachedProductTitle(Number(params.id));
   return { title: product?.title };
 }
 
@@ -43,11 +64,15 @@ export default async function ProductDetail({
   if (isNaN(id)) {
     return notFound();
   }
-  const product = await getProduct(id);
+  const product = await getCachedProduct(id);
   if (!product) {
     return notFound();
   }
   const isOwner = await getIsOwner(product.userId);
+  const revalidate = async () => {
+    "use server";
+    revalidateTag("product-title");
+  };
   return (
     <div>
       <div className="relative aspect-square">
@@ -84,9 +109,11 @@ export default async function ProductDetail({
           {formatToWon(product.price)}원
         </span>
         {isOwner ? (
-          <button className="rounded-md bg-red-500 px-5 py-2.5 font-semibold text-white">
-            Delete product
-          </button>
+          <form action={revalidate}>
+            <button className="rounded-md bg-red-500 px-5 py-2.5 font-semibold text-white">
+              Revalidate title cache
+            </button>
+          </form>
         ) : null}
         <Link
           className="rounded-md bg-orange-500 px-5 py-2.5 font-semibold text-white"
