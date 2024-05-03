0c2de162936415f059bf77ba8f219a1948be05ad
tedhun21 <songjihun940409@gmail.com>
2024-05-03 19:48:12 +0900

    #11.1 Optimistic Updates - See Posts

--- app/(tabs)/life/loading.tsx
+++ app/(tabs)/life/loading.tsx
@@ -7,7 +7,10 @@ export default function Loading() {
           <div className="flex flex-col gap-2 *:rounded-md">
             <div className="h-5 w-20 bg-neutral-700" />
             <div className="h-5 w-40 bg-neutral-700" />
-            <div className="h-5 w-10 bg-neutral-700" />
+            <div className="flex gap-2 *:rounded-md">
+              <div className="h-5 w-5 bg-neutral-700" />
+              <div className="h-5 w-5 bg-neutral-700" />
+            </div>
           </div>
         </div>
       ))}
--- app/(tabs)/life/page.tsx
+++ app/(tabs)/life/page.tsx
@@ -1,7 +1,63 @@
-export default function Life() {
+import db from "@/lib/db";
+import { formatToTimeAgo } from "@/lib/utils";
+import {
+  ChatBubbleBottomCenterIcon,
+  HandThumbUpIcon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+async function getPosts() {
+  const posts = await db.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      views: true,
+      created_at: true,
+      _count: {
+        select: { comments: true, likes: true },
+      },
+    },
+  });
+  return posts;
+}
+
+export const metadata = {
+  title: "동네생활",
+};
+
+export default async function Life() {
+  const posts = await getPosts();
+  console.log(posts);
   return (
-    <div>
-      <h1 className="text-4xl text-white">Life!</h1>
+    <div className="flex flex-col p-5">
+      {posts.map((post) => (
+        <Link
+          key={post.id}
+          href={`/posts/${post.id}`}
+          className="mb-5 flex flex-col gap-2 border-b border-neutral-500 pb-5 text-neutral-400 last:border-b-0 last:pb-0"
+        >
+          <h2 className="text-lg font-semibold text-white">{post.title}</h2>
+          <p>{post.description}</p>
+          <div className="flex items-center justify-between text-sm">
+            <div className="flex items-center gap-4">
+              <span>{formatToTimeAgo(post.created_at.toString())}</span>
+              <span>·</span>
+              <span>조회 {post.views}</span>
+            </div>
+            <div className="flex items-center gap-4 *:flex *:items-center *:gap-1">
+              <span>
+                <HandThumbUpIcon className="size-4" />
+                {post._count.likes}
+              </span>
+              <span>
+                <ChatBubbleBottomCenterIcon className="size-4" />
+                {post._count.comments}
+              </span>
+            </div>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
