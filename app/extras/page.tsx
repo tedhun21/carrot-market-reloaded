7e6e20edea0d2601e4c648307da4e32f60db6e48
tedhun21 <songjihun940409@gmail.com>
2024-05-20 20:28:56 +0900

    #13.4 Extras - Security

--- app/extras/page.tsx
+++ app/extras/page.tsx
@@ -1,24 +1,26 @@
-import { revalidatePath } from "next/cache";
+import HackedComponent from "@/components/hacked-component";
+import {
+  experimental_taintObjectReference,
+  experimental_taintUniqueValue,
+} from "react";
 
 async function getData() {
-  const data = await fetch(
-    "https://nomad-movies.nomadcoders.workers.dev/movies",
-  );
+  const keys = {
+    apiKey: "111919191",
+    secret: "1010101",
+  };
+  //experimental_taintObjectReference("API Keys were leaked!!!", secret);
+  experimental_taintUniqueValue("Secret key was exposed", keys, keys.secret);
+  return keys;
 }
 
 export default async function Extras() {
-  const action = async () => {
-    "use server";
-    revalidatePath("/extras");
-  };
-  await getData();
+  const data = await getData();
   return (
     <div className="flex flex-col gap-3 py-10">
       <h1 className="font-rubick text-6xl">Extras!</h1>
       <h2 className="font-roboto">So much more to learn!</h2>
-      <form action={action}>
-        <button>revalidate</button>
-      </form>
+      <HackedComponent data={data} />
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  logging: { fetches: { fullUrl: true } },
+  experimental: { taint: true },
   images: {
     remotePatterns: [{ hostname: "avatars.githubusercontent.com" }],
   },
